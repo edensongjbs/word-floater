44c8ec8499e8ef6f0a687fc0983f0bbc65874838
edensongjbs <edensongjbs@gmail.com>
2020-08-07 17:23:26 -0400

    added removal, but removing all items at once

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ import StaticInput from './StaticInput'
 
 class App extends React.Component {
   state = { arrayOfMovingInputs: []}
+  counter = 0
 
+  destroyWord = (id) => {
+    const newArray = [this.state.arrayOfMovingInputs].filter(e => e.id!==id)
+    this.setState({arrayOfMovingInputs: newArray}) 
+  }
+  
   spaceHandler = (word) => {
-    const newAr = [...this.state.arrayOfMovingInputs, word]
+    console.log(word)
+    const newWord = {word: word, id: this.counter++}
+    console.log(newWord)
+    const newAr = [...this.state.arrayOfMovingInputs, newWord]
+    // console.log(newAr)
     this.setState({arrayOfMovingInputs: newAr})
   }
 
   arrayOfMovingInputs = () => {
-    return this.state.arrayOfMovingInputs.map( (mi, index) => <MovingInput key={index} theString={mi}/>)
+    return this.state.arrayOfMovingInputs.map( mi => <MovingInput destroyWord={this.destroyWord} key={mi.id} theString={mi}/>)
   }
   
   render() {
--- src/MovingInput.js
+++ src/MovingInput.js
@@ -3,8 +3,7 @@ import React from 'react'
 export default class MovingInput extends React.Component {
     constructor() {
         super()
-        this.state = {left: "0px", top: "0px", 
-            interval: setInterval(() => this.updatePosition( Math.floor(Math.random()*5+1), Math.floor(Math.random()*5+1)), 100)}
+        this.state = {left: "0px", top: "0px"}
     }
 
     updatePosition = (x, y) => {
@@ -12,6 +11,7 @@ export default class MovingInput extends React.Component {
         let yNum=parseInt(this.state.top.replace("px",""))
         if (xNum > window.innerWidth || yNum > window.innerHeight) {
             clearInterval(this.state.interval)
+            this.props.destroyWord(this.props.theString.id)
             return
         }
         xNum+=x
@@ -19,9 +19,14 @@ export default class MovingInput extends React.Component {
         this.setState({left: `${xNum}px`, top: `${yNum}px`})
     }
 
+    componentDidMount() {
+        this.setState({interval: setInterval(() => this.updatePosition( Math.floor(Math.random()*5+1), Math.floor(Math.random()*5+1)), 100)})
+    }
+
     render() {
+        // console.log(this.props)
         return (
-            <p style={{position: "absolute", left:this.state.left, top: this.state.top }}>{this.props.theString}</p>
+            <p style={{position: "absolute", left:this.state.left, top: this.state.top }}>{this.props.theString.word}</p>
         )
     }
 }
